9e5cc55225ac4b5b2199fe441045b7f74d671870
Marv Odemwingie <odemwingiemarv@gmail.com>
2019-08-15 15:24:18 +0100

    Video Upload, User Sign Up and Sign In functional

--- include/classes/Account.php
+++ include/classes/Account.php
@@ -8,14 +8,14 @@ class Account {
     }
 
     public function login($uname, $pword) {
-        $password = hash("sha512", $password);
+        $pword = hash("sha512", $pword);
 
         // locating user in database
-        $query = $this->con->prepare("SELECT * FROM users WHERE username=:uname AND password:=pw");
+        $query = $this->con->prepare("SELECT * FROM users WHERE user_name=:uname AND password=:pword");
         $query->bindParam(":uname", $uname);
         $query->bindParam(":pword", $pword);
 
-        $query->execute():
+        $query->execute();
 
         // validating username and password
         if($query->rowCount() == 1) {
@@ -35,11 +35,177 @@ class Account {
         $this->validatePasswords($pass_word, $pass_word2);
 
         if(empty($this->errorArray)) {
-            return $this->insertUserDetails($first_name, $last_name, $user_name, $email, $pass_word)
+            return $this->insertUserDetails($first_name, $last_name, $user_name, $email, $pass_word);
         }
         else {
             return false;
         }
     }
+
+    public function updateDetails($first_name, $last_name, $user_name, $email, $pass_word) {
+        $this->validateFirstName($first_name);
+        $this->validateLastName($last_name);
+        $this->validateNewEmail($email, $user_name);
+
+        if(empty($this->errorArray)) {
+            $query = $this->con->prepare("UPDATE users SET firstName=:first_name, last_name=:last_name, email=:email WHERE user_name=:user_name");
+
+            $query->bindParam(":first_name", $first_name);
+            $query->bindParam(":last_name", $last_name);
+            $query->bindParam(":email", $email);
+            $query->bindParam(":user_name", $user_name);
+
+            return $query->execute();
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function updatePassword($oldPw, $pw, $pw2, $user_name) {
+        $this->validateoldPassword($oldPw, $user_name);
+        $this->validatePasswords($pw, $pw2);
+
+        if(empty($this->errorArray)) {
+            $query = $this->con->prepare("UPDATE users SET password=:pw WHERE user_name=:user_name");
+            $pw = hash("sha512", $pw);
+            $query->bindParam(":pw", $pw);
+            $query->bindParam(":user_name", $user_name);
+
+            return $query->execute();
+        }
+        else {
+            return false;
+        }
+    }
+
+    private function validateOldPassword($oldPw, $user_name) {
+        $pw = hash("sha512", $oldPw);
+
+        $query = $this->con->prepare("SELECT * FROM users WHERE user_name=:user_name AND password=:pw");
+        $query->bindParam(":user_name", $user_name);
+        $query->bindParam(":pw", $pw);
+
+        $query->execute();
+
+        if($query->rowCount() == 0) {
+            array_push($this->errorArray, Constants::$passwordIncorrect);
+        }
+    }
+
+    public function insertUserDetails($first_name, $last_name, $user_name, $email, $pass_word) {
+        $pass_word = hash("sha512", $pass_word);
+        $profilePic = "assets/images/profilePictures/default.png";
+
+        $query = $this->con->prepare("INSERT INTO users (first_name, last_name, user_name, email, password, profilePic) VALUES(:first_name, :last_name, :user_name, :email, :password, :pic)");
+
+        $query->bindParam(":first_name", $first_name);
+        $query->bindParam(":last_name", $last_name);
+        $query->bindParam(":user_name", $user_name);
+        $query->bindParam(":email", $email);
+        $query->bindParam(":password", $pass_word);
+        $query->bindParam(":pic", $profilePic);
+
+        return $query->execute();
+    }
+
+    private function validateFirstName($first_name) {
+        if(strlen($first_name) > 25 || strlen($first_name) < 2) {
+            array_push($this->errorArray, Constants::$firstNameCharacters);
+        }
+    }
+
+    private function validateLastName($last_name) {
+        if(strlen($last_name) > 25 || strlen($last_name) < 2) {
+            array_push($this->errorArray, Constants::$lastNameCharacters);
+        }
+    }
+
+    private function validateUsername($user_name) {
+        if(strlen($user_name) > 25 || strlen($user_name) < 2) {
+            array_push($this->errorArray, Constants::$usernameCharacters);
+            return;
+        }
+
+        $query = $this->con->prepare("SELECT user_name FROM users WHERE user_name=:user_name");
+        $query->bindParam(":user_name", $user_name);
+
+        $query->execute();
+
+        if($query->rowCount() != 0) {
+            array_push($this->errorArray, Constants::$usernameTaken);
+        }
+    }
+
+    private function validateEmails($email, $email2) {
+        if($email != $email2) {
+            array_push($this->errorArray, Constants::$emailsDoNotMatch);
+            return;
+        }
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
+
+        $query = $this->con->prepare("SELECT email FROM users WHERE email=:email");
+        $query->bindParam(":email", $email);
+
+        $query->execute();
+
+        if($query->rowCount() != 0) {
+            array_push($this->errorArray, Constants::$emailTaken);
+        }
+    }
+
+    private function validateNewEmail($email, $username) {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
+
+        $query = $this->con->prepare("SELECT email FROM users WHERE email=:email AND user_name!=:username");
+        $query->bindParam(":email", $email);
+        $query->bindParam(":username", $username);
+
+        $query->execute();
+
+        if($query->rowCount() != 0) {
+            array_push($this->errorArray, Constants::$emailTaken);
+        }
+    }
+
+    private function validatePasswords($password, $password2) {
+        if($password != $password2) {
+            array_push($this->errorArray, Constants::$passwordsDoNotMatch);
+            return;
+        }
+
+        if(preg_match("/[^A-Za-z0-9]/", $password)) {
+            array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
+            return;
+        }
+
+        if(strlen($password) < 5 || strlen($password) > 30) {
+            array_push($this->errorArray, Constants::$passwordLength);
+            return;
+        }
+    }
+
+    public function getError($error) {
+        if(in_array($error, $this->errorArray)) {
+            return "<span class='errorMessage'>$error</span>";
+        }
+    }
+
+    public function getFirstError() {
+        if(!empty($this->errorArray)) {
+            return $this->errorArray[0];
+        }
+        else {
+            return "";
+        }
+    }
+
 }
 ?>
--- sign_up.php
+++ sign_up.php
@@ -1,9 +1,78 @@
 <?php
+    require_once("include/header.php");
     require_once("include/config.php");
     require_once("include/classes/Account.php");
     require_once("include/classes/Constants.php");
-
-
+    require_once("include/classes/FormSanitizer.php");
 
     $account = new Account($con);
+
+    if(isset($_POST['submit_button'])) {
+        $first_name = FormSanitizer::sanitizeFormString($_POST["first_name"]);
+        $last_name = FormSanitizer::sanitizeFormString($_POST["last_name"]);
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $email = FormSanitizer::sanitizeFormUsername($_POST["email"]);
+        $email2 = FormSanitizer::sanitizeFormUsername($_POST["email2"]);
+        $password = FormSanitizer::sanitizeFormUsername($_POST["password"]);
+        $password2 = FormSanitizer::sanitizeFormUsername($_POST["password2"]);
+
+        $wasSuccessful = $account->register($first_name, $last_name, $username, $email, $email2, $password, $password2);
+
+        if($wasSuccessful) {
+            $_SESSION["userLoggedIn"] = $username;
+            header("Location: index.php");
+        }
+    }
+
+
+    function getInputValue($name){
+        if(isset($_POST[$name])){
+            echo $_POST[$name];
+        }
+    }
+
 ?>
+
+<div class="row">
+    <div class="offset-md-2 offset-lg-3 mt-5"></div>
+    <div class=" col-lg-6 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h2>Sign Up</h2>
+            </div>
+            <div class="card-body">
+                <form action="sign_up.php" method="POST">
+
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                    <input class="form-control" type="text" name="first_name" placeholder="First Name" value="<?php getInputValue("first_name") ?>" required><br>
+
+                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                    <input class="form-control" type="text" name="last_name" placeholder="Last Name" value="<?php getInputValue("last_name") ?>" required><br>
+
+                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                    <?php echo $account->getError(Constants::$usernameTaken); ?>
+                    <input class="form-control" type="text" name="username" placeholder="Username" value="<?php getInputValue("username") ?>" required><br>
+
+                    <?php echo $account->getError(Constants::$emailTaken); ?>
+                    <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                    <?php echo $account->getError(Constants::$emailInvalid); ?>
+                    <input class="form-control" type="email" placeholder="Email" name="email" value="<?php getInputValue("email"); ?>" required autocomplete="off"><br>
+                    <input class="form-control" type="email" placeholder="Confirm Email" name="email2" value="<?php getInputValue("email2"); ?>" required autocomplete="off"><br>
+
+                    <?php echo $account->getError(Constants::$passwordLength); ?>
+                    <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                    <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                    <input class="form-control" type="password" placeholder="Password" name="password" value="<?php getInputValue("password"); ?>" required autocomplete="off"><br>
+                    <input class="form-control" type="password" placeholder="Confirm Password" name="password2" value="<?php getInputValue("password2"); ?>" required autocomplete="off"><br>
+
+                    <input class="btn btn-primary" type="submit" name="submit_button" value="Submit">
+                </form>
+            </div>
+            <div class="card-footer">
+                <a href="sign_in.php">Already have an account? Sign in here!</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="offset-md-2 offset-lg-3"></div>
+</div>
